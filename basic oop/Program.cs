2cabb0d46da105e15001997f2dbb54da0df2e125
LouisA-W <54893198+LouisA-W@users.noreply.github.com>
2020-03-21 18:37:00 +0000

    Add files via upload

---
+++ basic oop/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_oop
+{
+    class Program
+    {
+        static void feed(string state, int size)
+        {
+            size++;
+            Console.WriteLine("Fish fed");
+            if (size == 5)
+            {
+                state = "Fish";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Animal thisFish = new Animal("Fish", 1);
+            Console.WriteLine("{0} is of size {1}", thisFish.getState(), thisFish.getSize());
+            while(thisFish.getState() != "Fish")
+            {
+                thisFish.feed();
+            }
+            Console.WriteLine("It is now a big {0}", thisFish.getState());
+
+            string thisFishState = "Fish";
+            int thisFishSize = 1;
+            Console.WriteLine("{0} is of size {1}", thisFishState, thisFishSize);
+            while (thisFishState != "Fish")
+            {
+                feed(thisFishState, thisFishSize);
+            }
+            Console.WriteLine("It is now a big {0}", thisFishState);
+        }
+    }
+    class Animal
+    {
+        private string state;
+        private int size;
+        public Animal(string s, int n)
+        {
+            state = s;
+            size = n;
+        }
+        public void feed()
+        {
+            size++;
+            state = "Fish";
+
+        }
+        public string getState() => state;
+        public int getSize() => size;
+
+
+    }
+}
---
+++ cars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cars
+{
+    class Program
+    {
+        class Car
+        {
+            private string Registration;
+            private string Make;
+            private string DateOfInspection;
+            private int Mileage;
+            public Car(string r, string m)
+            {
+                Registration = r;
+                Make = m;
+                Mileage = 0;
+            }
+            public string getMake() => Make;
+            public int getMileage() => Mileage;
+            public string getReg() => Registration;
+            public string getDate() => DateOfInspection;
+            void setInspectionDate(string date, int mileage)
+            {
+                date = getDate();
+                mileage = getMileage();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
